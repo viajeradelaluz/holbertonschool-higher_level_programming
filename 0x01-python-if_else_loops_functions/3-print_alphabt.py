3e87565115c76f840170e10df40de2bf81a1006a
Johanna Alfonso <jonna.alfonso@gmail.com>
2021-12-21 16:23:32 -0500

    Fix ASCII number for 'q'

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for character in range(97, 123):
-    if character == 101 or character == 123:
+    if character == 101 or character == 113:
         continue
     print("{:c}".format(character), end="")
