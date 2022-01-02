d65f23ec3bbc9ae5aac4fc55e9939eb6d7043e59
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-02 14:58:24 -0500

    feat: use command line arguments [argv()] as an importable module

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv)
+
+    if argc == 1:
+        word = "arguments."
+    elif argc == 2:
+        word = "argument:"
+    else:
+        word = "arguments:"
+
+    print("{:d} {:s}".format(argc - 1, word))
+
+    for num, value in enumerate(argv):
+        if num > 0:
+            print("{:d}: {:s}".format(num, value))
