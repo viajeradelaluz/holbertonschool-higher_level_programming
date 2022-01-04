505104c7540d252d8d198204b1e9a3847de79002
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-03 19:05:35 -0500

    fix: check if my_list exists

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if not idx >= len(my_list) or not idx < 0:
-        del my_list[idx]
+    if my_list:
+        if not idx >= len(my_list) or not idx < 0:
+            del my_list[idx]
 
     return my_list
