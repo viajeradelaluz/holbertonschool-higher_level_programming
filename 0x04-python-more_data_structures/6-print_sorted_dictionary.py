9ff32b5e5f3145ac9189b04cca125bf09ffcd5e3
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-05 10:49:26 -0500

    feat: print a dictionary by ordered keys

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+
+    new_dict = sorted(a_dictionary.keys())
+    for key in new_dict:
+        print("{}: {}".format(key, a_dictionary[key]))
