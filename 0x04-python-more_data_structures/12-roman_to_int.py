2fb2e6c8ba99365ba38bbc44625d27c0c413e4ce
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-05 20:14:13 -0500

    feat: convert roman numbers to arabic numbers

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string and type(roman_string) is not str:
+        return (0)
+
+    roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
+
+    int_num = []
+    for letter in roman_string:
+        for symbol in roman_num.keys():
+            if letter == symbol:
+                int_num.append(roman_num.get(symbol, None))
+
+    last = len(int_num)
+    if last >= 1:
+        if int_num[last - 1] > int_num[last - 2]:
+            int_num[last - 2] *= -1
+            return sum(int_num)
+
+    return sum(int_num)
