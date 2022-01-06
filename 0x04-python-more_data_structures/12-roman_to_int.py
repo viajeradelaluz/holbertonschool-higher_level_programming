06ed2675670cd5d68a07334645ef420713c793bd
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-05 20:28:05 -0500

    fix: handle empty strings

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string and type(roman_string) is not str:
+    if not roman_string or type(roman_string) is not str:
         return 0
 
     roman_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
