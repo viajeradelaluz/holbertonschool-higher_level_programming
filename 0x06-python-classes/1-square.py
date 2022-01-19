fac13ea6f200dea18b2ce89cfbe2cdf02e807802
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-18 21:42:28 -0500

    feat: set the square class

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Set the Square class """
+
+
+class Square:
+    """ Defines 'size' as private instance attribute """
+
+    def __init__(self, size):
+        self.__size = size
