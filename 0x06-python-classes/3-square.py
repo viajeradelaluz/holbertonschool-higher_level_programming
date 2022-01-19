0e47eefd1ab5c12787000a1fee959ed90426e065
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-18 22:48:02 -0500

    feat: set one public instance method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Set the Square class """
+
+
+class Square:
+    """ Defines 'size' as private instance attribute """
+
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
