a86d9aac7e7b0372f451e74d7cb052fea8120240
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-18 23:03:46 -0500

    feat: set private instance attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Set the Square class """
+
+
+class Square:
+    """ Defines 'size' as private instance attribute """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int):
+            if value >= 0:
+                self.__size = value
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return self.__size ** 2
