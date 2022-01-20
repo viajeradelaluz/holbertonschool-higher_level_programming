2fe1ee820644a83412477bcee842ce0847b2f0b0
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-19 21:46:56 -0500

    fix: change the comment's place

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -3,19 +3,19 @@ import math
 
 
 class MagicClass:
-    """ Raising the power of beating the bytecode """
 
+    """ Raising the power of beating the bytecode.
+    Define the radius attribute """
     def __init__(self, radius=0):
-        """ Define the radius attribute """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
         self.__radius = radius
 
+    """ Set the power of radius and math.pi """
     def area(self):
-        """ Set the power of radius and math.pi """
         return self.__radius ** 2 * math.pi
 
+    """ Circum the radius """
     def circumference(self):
-        """ Circum the radius """
         return 2 * math.pi * self.__radius
