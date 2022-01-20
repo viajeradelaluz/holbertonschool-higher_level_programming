6d824ac0537168ad20a98accbc2933e2bb059731
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-19 21:31:09 -0500

    feat: beating the bytecode on python

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+    """ Raising the power of beating the bytecode """
+
+    def __init__(self, radius=0):
+        """ Define the radius attribute """
+        self.radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """ Set the power of radius and math.pi """
+        return self.__radius ** 2 * math.pi
+
+    def circumference(self):
+        """ Circum the radius """
+        return self.__radius * math.pi * 2
