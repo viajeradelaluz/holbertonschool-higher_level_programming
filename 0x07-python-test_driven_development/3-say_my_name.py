294ad16988176c0706872c4df5b95c4a380fb452
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-25 13:33:33 -0500

    feat: method to print a name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Say my name
+
+This module contains one function that prints a name.
+
+    """
+
+
+def say_my_name(first_name, last_name=""):
+    """ Print first and last name """
+
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+
+    print("My name is", first_name, last_name)
