d3a5f014262d9035ea50ce77fa8e92b37fa3fb34
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-24 20:38:14 -0500

    feat: set message for __del__ destructor

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python3
+""" Rectangle
+
+Define the class Rectangle
+    """
+
+
+class Rectangle:
+    """ Representation of a rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ Initialize the rectangle """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Method that returns the width of the retangle """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Method that sets the width of the rectangle """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Method that returns the height of the retangle """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Method that sets the height of the rectangle """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Method to calculate the rectangle area """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Method to calculate the rectangle perimeter"""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        """ Method to represent the rectangle with # """
+
+        to_print = ""
+        if self.__width != 0 and self.__height != 0:
+            to_print = ("#" * self.__width + "\n") * self.__height
+            to_print = to_print[:-1]
+
+        return to_print
+
+    def __repr__(self):
+        """ Extract the representation of the rectangle with repr """
+        rep = "Rectangle({self.width}, {self.height})".format(self=self)
+        return rep
+
+    def __del__(self):
+        """ Set message for __del__ destructor """
+        print("Bye rectangle...")
