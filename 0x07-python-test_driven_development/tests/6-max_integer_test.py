8a562d113ce9c8e4d3313f6e6d7db899c0e49aa3
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-26 00:11:41 -0500

    fix: add mores test

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -39,3 +39,38 @@ class TestMaxInteger(unittest.TestCase):
     def test_without_list(self):
         with self.assertRaises(TypeError):
             max_integer(29, 25, 18, 22)
+
+    def test_max_int(self):
+        # Test max integers when passed lists and digits
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([2, 5, 9, 5]), 9)
+        self.assertEqual(max_integer([9, 5]), 9)
+        self.assertEqual(max_integer([0]), 0)
+        self.assertEqual(max_integer([-6, -9, -2, 0]), 0)
+        self.assertEqual(max_integer([-6, -9, -2, -3]), -2)
+        self.assertEqual(max_integer((2, 4)), 4)
+
+    def test_max_string(self):
+        self.assertEqual(max_integer(["5", "7", "9"]), "9")
+        self.assertEqual(max_integer(["Hello", "Good", "Bye"]), "Hello")
+        self.assertEqual(max_integer("5"), "5")
+        self.assertEqual(max_integer("4567"), "7")
+        self.assertEqual(max_integer("abcxyz"), "z")
+        self.assertEqual(max_integer(["a", "b", "c", "d", "e", "y", "z"]), "z")
+        self.assertEqual(max_integer(["abc", "y"]), "y")
+
+    def test_list_floats(self):
+        self.assertEqual(max_integer([[1, 2], [2, 3]]), [2, 3])
+        self.assertEqual(max_integer([{1, 2}, {2}, {3, 4}]), {1, 2})
+        self.assertEqual(max_integer([-6.1, -9.8, -2.5, -3.7]), -2.5)
+
+    def test_None(self):
+        # Test max integers when list is empty
+        self.assertIsNone(max_integer([]), None)
+        self.assertIsNone(max_integer(), None)
+        self.assertIsNone(max_integer([None]), None)
+
+    def test_errors(self):
+        # Make sure value errors are raised when necessary
+        self.assertRaises(TypeError, max_integer, 1)
+        self.assertRaises(TypeError, max_integer, 1, 5)
