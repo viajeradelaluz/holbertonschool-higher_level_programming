1d67a80d32ce7dbcb5a8c5afea8547fbb1258066
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-31 18:07:46 -0500

    feat: set are method for the BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Improve Geometry
+
+Contains the class BaseGeometry.
+    """
+
+
+class BaseGeometry():
+    """ Define public attribute area  """
+
+    def area(self):
+        """ Raises an Exception """
+        raise Exception("area() is not implemented")
