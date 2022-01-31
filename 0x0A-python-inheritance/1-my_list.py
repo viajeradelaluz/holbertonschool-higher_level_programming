aef4da3712193818323bfa5eed33f630c2213a1c
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-01-31 18:53:34 -0500

    fix: add comments to the methods

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+""" My list
+
+Contains the class MyList.
+    """
+
 
 class MyList(list):
+    """ Define a class thar inherits from list """
 
     def __init__(self):
+        """ Builder of the mylist class """
         super().__init__()
 
     def print_sorted(self):
+        """ Order the list """
         list_copy = self.copy()
         list_copy.sort()
         print(list_copy)
