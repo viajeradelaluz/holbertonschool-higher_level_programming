117d2d800b947c5f37ba9a7e5888270efede0021
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 21:44:48 -0500

    feat: save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Save Object to a file 
+
+Contains the save_to_json_file method.
+    """
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Returns JSON representation of an str object """
+    with open(filename, 'w', encoding='utf-8') as f:
+        return json.dump(my_obj, f)
