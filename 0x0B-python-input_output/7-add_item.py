d0b77d3ba2e0893d3f50f3c2a0e25d620c11f241
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 21:58:46 -0500

    fix: unset argv outside a variable

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,11 +10,10 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
-arguments = sys.argv[1:]
 
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
 
-save_to_json_file(my_list + arguments, filename)
+save_to_json_file(my_list + sys.argv[1:], filename)
