038ab6b46bdd3b545cef4665006dd3fd303293c1
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 21:45:43 -0500

    feat: add item

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Load, add, save
+
+Script that adds arguments to a Python list, and then save them to a file.
+    """
+
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+arguments = sys.argv[1:]
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+save_to_json_file(my_list + arguments, filename)
