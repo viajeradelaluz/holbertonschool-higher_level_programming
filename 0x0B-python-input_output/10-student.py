12e26b331910d1b58151d8d7f4ad0ad0b49b13e6
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 21:47:13 -0500

    feat: more methos to student class

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Student to JSON with filter
+    """
+
+
+class Student():
+    """ Defines a student info """
+
+    def __init__(self, first_name, last_name, age):
+        """ Builder of the Student class """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Returns a dictionary representation of a Student """
+
+        new_dict = {}
+
+        if attrs == None:
+            return self.__dict__
+
+        for attr in attrs:
+            if attr in self.__dict__.keys():
+                new_dict[attr] = self.__dict__[attr]
+
+        return new_dict
