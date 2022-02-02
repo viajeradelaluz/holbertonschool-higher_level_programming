2fb31142f6fda30295b41a524059c66e22776b22
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 22:16:07 -0500

    fix: set file as executable

--- 7-add_item.py
+++ 7-add_item.py
@@ -10,9 +10,9 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+
 try:
     my_list = load_from_json_file(filename)
 except FileNotFoundError:
     my_list = []
-
 save_to_json_file(my_list + sys.argv[1:], filename)
