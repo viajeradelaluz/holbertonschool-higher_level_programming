2625bfb58952b168cbd3938c1617d0906a524ed2
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-02-01 21:49:59 -0500

    fix: calling filename

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,5 +7,5 @@ Contains the write_file method.
 
 def write_file(filename="", text=""):
     """ Returns the number of characters written """
-    with open("my_first_file.txt", 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write("This School is so cool!\n")
