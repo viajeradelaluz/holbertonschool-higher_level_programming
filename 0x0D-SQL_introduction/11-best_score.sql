0293167fc9ae58e225cc2f77f09f893e25d3bcaa
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-03-14 18:24:58 -0500

    feat: sorting fields by filters

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- Lists records with a score >= 10 in the second_table (database hbtn_0c_0)
+-- * Results should display both the score and the name (in this order)
+-- * Records should be ordered by score (top first)
+SELECT score, name FROM second_table WHERE score >= 10
+ORDER BY score DESC;
