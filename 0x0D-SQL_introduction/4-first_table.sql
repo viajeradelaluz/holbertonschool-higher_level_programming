0fce2baa5686cefa39c942d6bfabf711cb20bbe5
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-03-14 18:22:36 -0500

    feat: create table into a database

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Creates a new table in a current database 
+-- The database name will be passed as an argument of the mysql command
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- Prints the full description of the table first_table
+-- From a given database as argument
+SHOW CREATE TABLE first_table;
