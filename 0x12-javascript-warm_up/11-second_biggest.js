5874010642e321a60195924c39001f4f9e1695f9
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-04 23:02:07 -0500

    fix: change execution order

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-if (args.length <= 1) {
-  console.log(0);
-} else {
+if (args.length > 1) {
   args.sort();
   console.log(args[args.length - 2]);
 }
+console.log(0);
