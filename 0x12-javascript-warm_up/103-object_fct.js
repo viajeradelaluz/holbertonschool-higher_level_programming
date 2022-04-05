1705ff0b8994417cfe832779b60a5a94c7f9b082
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-05 00:00:15 -0500

    feat: increment object

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,11 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function () {
+
+myObject.incr = () => {
   myObject.value++;
 };
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
