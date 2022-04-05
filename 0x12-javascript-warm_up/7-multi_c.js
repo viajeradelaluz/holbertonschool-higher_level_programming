1b2ed34a37b7c448272aaf40600b37d40f155b2e
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-04 20:55:41 -0500

    fix: replace substraction of times

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,8 @@ let times = parseInt(process.argv[2]);
 if (isNaN(times)) {
   console.log('Missing number of occurrences');
 } else {
-  while (times--) {
+  while (times > 0) {
     console.log('C is fun');
+    times--;
   }
 }
