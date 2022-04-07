12b7d49e74c6d2962b1b5df01da4d0175519ff4e
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-07 14:21:56 -0500

    feat: class Square #1

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let row = 0; row < this.width; row++) {
+        console.log('c'.repeat(this.height));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
