42d8fdff9adebf24199fe1378af9463f8b5d2771
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-04-07 13:53:03 -0500

    feat: class Rectangle #4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let row = 0; row < this.height; row++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const wValue = this.width;
+    this.width = this.height;
+    this.height = wValue;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+module.exports = Rectangle;
