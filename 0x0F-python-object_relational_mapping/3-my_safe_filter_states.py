3797832534352bac93395f95c73ca6760aa57b4b
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-14 10:14:41 -0500

    fix: port as int

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,12 +19,12 @@ from sys import argv
 def main():
     db = MySQLdb.connect(
         user=argv[1], passwd=argv[2], db=argv[3],
-        host='localhost', port='3306')
+        host='localhost', port=3306)
 
     cursor = db.cursor()
     cursor.execute(
-        'SELECT * FROM states WHERE name LIKE %s'
-        'ORDER BY id ASC', (argv[4]))
+        'SELECT * FROM states WHERE name = "{}"'
+        'ORDER BY id ASC'.format(argv[4]))
 
     states = cursor.fetchall()
     for state in states:
