a0ab11844ab4f949a2b0c7fb4ee671c9c24f2f7c
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-13 23:51:15 -0500

    feat: filter states by user input

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" This module solves the following requirements and relies on the file
+    task/0-select_states.sql
+
+Write a script that takes in an argument and displays all values in the
+states table of hbtn_0e_0_usa where name matches the argument:
+    - Take 4 arguments: mysql username, mysql password, database name
+      and state name searched
+    - You must use the module MySQLdb (import MySQLdb)
+    - Connect to a MySQL server running on localhost at port 3306
+    - Results must be sorted in ascending order by states.id
+"""
+
+import MySQLdb
+from sys import argv
+
+
+def main():
+    db = MySQLdb.connect(
+        user=argv[1], passwd=argv[2], db=argv[3],
+        host='localhost', port=3306)
+
+    cursor = db.cursor()
+    cursor.execute(
+        f'SELECT * FROM states WHERE name = "{argv[4]}" ORDER BY id ASC;')
+
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    main()
