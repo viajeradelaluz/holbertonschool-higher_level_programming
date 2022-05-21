15a0342d742c16482acb66a92bf418597b3ee9dd
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-21 10:41:55 -0500

    fix: pycodestyle

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,19 +5,17 @@
 Write a function that finds a peak in a list of unsorted integers.
     - Prototype: def find_peak(list_of_integers):
     - You are not allowed to import any module
-    - Your algorithm must have the lowest complexity (hint: you don't need to go
-      through all numbers to find a peak)
+    - Your algorithm must have the lowest complexity (hint: you don't need to
+      go through all numbers to find a peak)
     - 6-peak.py must contain the function
-    - 6-peak.txt must contain the complexity of your algorithm: O(log(n)), O(n),
-      O(nlog(n)) or O(n2)
+    - 6-peak.txt must contain the complexity of your algorithm: O(log(n)),
+      O(n), O(nlog(n)) or O(n2)
     - Note: there may be more than one peak in the list
 """
 
 
 def find_peak(list_of_integers):
     """ Find a peak in an unsorted list of integers """
-    if len(list_of_integers) == 0:
-        return None
-
-    list_of_integers.sort()
-    return list_of_integers[-1]
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
