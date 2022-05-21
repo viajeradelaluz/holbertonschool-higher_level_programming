7001bbdfb0bb5b43a9edf5e964afb652c296ec52
Johanna Alfonso <jonna.alfonso@gmail.com>
2022-05-21 10:37:36 -0500

    fix: using sort() function

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -15,16 +15,9 @@ Write a function that finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
-    """ Find a peak without going to all numbers """
+    """ Find a peak in an unsorted list of integers """
     if len(list_of_integers) == 0:
         return None
 
-    peak, temp, i = 0, 0, 1
-
-    while i < len(list_of_integers):
-        left = list_of_integers[i - 1]
-        right = list_of_integers[i]
-        temp = left if left > right else right
-        peak = temp if temp > peak else peak
-        i *= 2
-    return peak
+    list_of_integers.sort()
+    return list_of_integers[-1]
